fa403a50949c40e3dac4fa00b6caf317e6e71dcc
Nilesh <nileshmaurya2004@gmail.com>
2026-08-01 23:34:29 +0530

    Update API base URL to Render

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,8 @@
 import axios from "axios" ;
 
-export const BASE_URL = "http://localhost:9090"
+
+
+export const BASE_URL = "https://proconnectplatform.onrender.com"
 
 export const clientServer = axios.create({
     baseURL : BASE_URL,
